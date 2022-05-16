1b9495c29697d84fd08e6608d282676a6baeaca9
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2022-05-16 18:43:53 +0300

    config user name base

--- rechauffement_climatique_BO/connexion.php
+++ rechauffement_climatique_BO/connexion.php
@@ -1,7 +1,7 @@
 <?php
     $host = 'ec2-54-86-224-85.compute-1.amazonaws.com';
     $dbname = 'ddi6usnvt9bes6';
-    $username = 'postgresqzlxhsghigmhbb';
+    $username = 'qzlxhsghigmhbb';
     $password = 'dd7a9bfa463fc4a59bd5a663be7954e48c1050a040aba99f2950d3e2f60d4013';
  
   $dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
