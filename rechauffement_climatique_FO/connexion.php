358935617778f8cce9ee17ef5793c541b689729f
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2022-05-16 15:56:39 +0300

    config connexion

--- rechauffement_climatique_FO/connexion.php
+++ rechauffement_climatique_FO/connexion.php
@@ -1,8 +1,8 @@
 <?php
-    $host = 'localhost';
-    $dbname = 'rechauffement';
-    $username = 'postgres';
-    $password = 'mdpprom13';
+    $host = 'ec2-54-86-224-85.compute-1.amazonaws.com';
+    $dbname = 'ddi6usnvt9bes6';
+    $username = 'qzlxhsghigmhbb';
+    $password = 'dd7a9bfa463fc4a59bd5a663be7954e48c1050a040aba99f2950d3e2f60d4013';
  
   $dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
    
--- rechauffement_climatique_FO/index.php
+++ rechauffement_climatique_FO/index.php
@@ -14,7 +14,7 @@
   <head>
   	<title>Website menu 09</title>
     <meta charset="utf-8">
-	<base href="http://localhost/s6/TP-S6-P13-Web-design-Mai-2022/rechauffement_climatique_FO/" />
+	<base href="http://rechauf.herokuapp.com/rechauffement_climatique_FO/" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
