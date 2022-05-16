438c0db0c732b0a59d5ef5ebb143b282ea3f3069
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2022-05-16 19:20:30 +0300

    optimisation

--- bdd.sql
+++ bdd.sql
@@ -31,6 +31,7 @@ CREATE TABLE actualite(
     FOREIGN KEY(idCategorie) REFERENCES categorie (id),
     FOREIGN KEY(idLieu) REFERENCES lieu (id)
 );*/
+delete from actualite;
 
 INSERT INTO actualite (idCategorie, date, idLieu, titre, resume, contenu, url, photo) VALUES (
     1, 
@@ -40,13 +41,9 @@ INSERT INTO actualite (idCategorie, date, idLieu, titre, resume, contenu, url, p
     'Nantes a enregistré 48,2 mm de pluie en vingt-quatre heures, après l’épisode orageux qui s’est abattu sur la ville, dimanche 24 avril dans l’après-midi. Une quantité remarquée, mais pas rare selon Météo France', 'Le phénomène a été très localisé sur Nantes, où un violent orage s’est abattu vers 16 h, dimanche 24 avril, mêlant pluie, grêle et foudre. La station automatique Météo France de Nantes-Bouguenais a relevé 48,2 mm de précipitations, tandis que le point suivant était à 15 mm seulement, à Ponchâteau.
     « Sur un mois, on enregistre normalement environ une soixantaine de millimètres, précise Météo France. Dimanche, il est tombé l’équivalent de trois semaines de précipitations ».
     Pour autant, le phénomène n’est pas exceptionnel. « C’est une quantité remarquée, mais ce n’est pas rare sous orage, souligne Météo France. Et les orages ne sont pas rares en mars – avril. » Et d’expliquer : « Ils sont dus à une différence de température. Nous avions de l’air doux accumulé par des températures élevées ces derniers jours, avec de l’air froid en altitude. »',
-    'france/nantes-l-equivalent-de-trois-semaines-de-precipitations-est-tombe-pendant-l-orage-de-dimanche',
+    'nantes-l-equivalent-de-trois-semaines-de-precipitations-est-tombe-pendant-l-orage-de-dimanche',
     'innondation1.jpg');
 
-drop table definition;
-drop table cause;
-drop table consequence;
-drop table solution;
 
 /*CREATE TABLE Login(
     id serial primary key,
