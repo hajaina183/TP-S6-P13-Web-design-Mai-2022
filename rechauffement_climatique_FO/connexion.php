39f089809fd1615457e1551ce4b39f22d6bc3cf9
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2022-05-17 12:48:13 +0300

    config base

--- rechauffement_climatique_FO/connexion.php
+++ rechauffement_climatique_FO/connexion.php
@@ -1,8 +1,8 @@
 <?php
-    $host = 'ec2-54-86-224-85.compute-1.amazonaws.com';
-    $dbname = 'ddi6usnvt9bes6';
-    $username = 'qzlxhsghigmhbb';
-    $password = 'dd7a9bfa463fc4a59bd5a663be7954e48c1050a040aba99f2950d3e2f60d4013';
+    $host = 'ec2-54-211-255-161.compute-1.amazonaws.com';
+    $dbname = 'd9vg3r1r4eo0k3';
+    $username = 'hsmltsdsoihphh';
+    $password = '978b8db7a694f1e8e1c6c88cdbb6a84da0147f74ce6a278fad371cee39802ed8';
  
   $dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
    
